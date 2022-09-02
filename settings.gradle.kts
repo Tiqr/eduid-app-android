987be08325438c88781e21d0be611f3da0d1449c
Wiebe Elsinga <w.elsinga@gmail.com>
2022-09-02 13:41:38 +0200

    TIQR: Use library by github submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,14 @@ rootProject.name = "eduid app android"
 
 include(":app")
 
+// Add core
+include(":core")
+project(":core").projectDir = File("app-core/core")
+
+// Add data
+include(":data")
+project(":data").projectDir = File("app-core/data")
+
 // Enable Gradle's version catalog support
 // https://docs.gradle.org/current/userguide/platforms.html
 enableFeaturePreview("VERSION_CATALOGS")
