63fbaaf7af3c53ff31ed82bea7aa14ecd99c08c9
Iulia Stana <iulia@egeniq.com>
2023-02-01 11:29:32 +0100

    Reverting the experimental LoginViewModel.kt logic using the experimental core code as it's not a working solution. Updating the core to the same version as the eduid develop branch.

--- app/src/main/kotlin/nl/eduid/screens/login/LoginViewModel.kt
+++ app/src/main/kotlin/nl/eduid/screens/login/LoginViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.tiqr.data.model.SecretCredential
 import org.tiqr.data.repository.IdentityRepository
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,22 +15,27 @@ class LoginViewModel @Inject constructor(private val repository: IdentityReposit
     val loginValid = MutableLiveData<Boolean?>(null)
 
     fun isLoginValid(credential: SecretCredential) = viewModelScope.launch {
-        val identities = repository.getAllIdentities()
-        if (identities.isEmpty()) {
-            return@launch
-        } else {
-            try {
-                val canDecrypt = repository.canDecryptWithPassword(
-                    identities[0], credential.password, credential.type
-                )
-                loginValid.postValue(true)
-                Timber.e("Can decrypt $canDecrypt")
-            } catch (e: Exception) {
-                loginValid.postValue(false)
-                Timber.e("Decryption failed. Invalid password.")
-            }
+        delay(1000L)
+        loginValid.postValue(true)
 
-        }
+        //This was a check to see if the existing PIN can be used as an offline app unlock mechanism
+        //experimental changes to the core were made. Since this is not a working solution,
+        // I'm reverting this for now and keeping the core the same as on develop
+//        val identities = repository.getAllIdentities()
+//        if (identities.isEmpty()) {
+//            return@launch
+//        } else {
+//            try {
+//                val canDecrypt = repository.canDecryptWithPassword(
+//                    identities[0], credential.password, credential.type
+//                )
+//                loginValid.postValue(true)
+//                Timber.e("Can decrypt $canDecrypt")
+//            } catch (e: Exception) {
+//                loginValid.postValue(false)
+//                Timber.e("Decryption failed. Invalid password.")
+//            }
+//        }
     }
 
 }
