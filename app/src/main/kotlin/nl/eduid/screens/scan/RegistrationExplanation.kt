2aaa1123615d79a0c85c0f116049f568dc7b2220
Dániel Zolnai <daniel@egeniq.com>
2023-12-08 10:07:01 +0100

    TIQR-381: Fix HTML text references

--- app/src/main/kotlin/nl/eduid/screens/scan/RegistrationExplanation.kt
+++ app/src/main/kotlin/nl/eduid/screens/scan/RegistrationExplanation.kt
@@ -4,19 +4,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import de.charlex.compose.material3.HtmlText
 import nl.eduid.R
-import nl.eduid.ui.annotatedStringWithBoldParts
 import nl.eduid.ui.theme.EduidAppAndroidTheme
 
 @Composable
@@ -25,14 +22,10 @@ fun RegistrationExplanation(modifier: Modifier = Modifier) = Column(
     verticalArrangement = Arrangement.Bottom,
     modifier = modifier
 ) {
-    Text(
-        text = annotatedStringWithBoldParts(
-            stringResource(R.string.ScanView_MainText_COPY),
-            stringResource(R.string.ScanView_MainTextBoldPart_COPY),
-            boldWeight = FontWeight.Bold
-            ),
+    HtmlText(
+        text = stringResource(R.string.ScanView_MainText_COPY),
         style = MaterialTheme.typography.titleLarge.copy(
-            textAlign = TextAlign.Center, color = Color.White, fontWeight = FontWeight.SemiBold
+            textAlign = TextAlign.Center, color = Color.White, fontWeight = FontWeight.Normal
         ),
         modifier = Modifier.fillMaxWidth()
     )
--- app/src/main/kotlin/nl/eduid/ui/TermsAndConditions.kt
+++ app/src/main/kotlin/nl/eduid/ui/TermsAndConditions.kt
@@ -1,20 +1,14 @@
 package nl.eduid.ui
 
-import android.text.Html
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import de.charlex.compose.material3.HtmlText
 import nl.eduid.R
 import nl.eduid.ui.theme.EduidAppAndroidTheme
 
@@ -37,7 +31,7 @@ fun CheckToSAndPrivacyPolicy(
                 .width(16.dp)
         )
         HtmlText(
-            html = stringResource(id = R.string.LinkFromInstitution_AgreeWithTerms_COPY),
+            text = stringResource(id = R.string.LinkFromInstitution_AgreeWithTerms_COPY),
             modifier = Modifier
                 .wrapContentWidth()
                 .padding(start = 12.dp)
