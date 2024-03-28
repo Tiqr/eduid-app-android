ea185abda85751d245d05ecd136d3a43273294ec
Iulia STANA <iulia@egeniq.com>
2024-03-28 10:39:10 +0100

    TIQR-403: Add new line in text

--- app/src/main/kotlin/nl/eduid/screens/firsttimedialog/FirstTimeDialogScreen.kt
+++ app/src/main/kotlin/nl/eduid/screens/firsttimedialog/FirstTimeDialogScreen.kt
@@ -31,11 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nl.eduid.R
@@ -149,7 +145,7 @@ private fun FirstTimeDialogContent(
                         stringResource(id = R.string.CreateEduID_FirstTimeDialog_MainText_COPY),
                         stringResource(id = R.string.CreateEduID_FirstTimeDialog_MainTextFirstBoldPart_COPY),
                         stringResource(id = R.string.CreateEduID_FirstTimeDialog_MainTextSecondBoldPart_COPY),
-                        ),
+                    ),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 24.dp)
