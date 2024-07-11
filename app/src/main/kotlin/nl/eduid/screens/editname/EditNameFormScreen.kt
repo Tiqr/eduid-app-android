f39b52a305bec4f7078346ea5788b10c16bb0c47
Iulia STANA <iulia@egeniq.com>
2024-07-11 13:25:17 +0200

    TIQR-426: Remove wrong text body on edit name screen. Removed animation for hiding title when keyboard is visible.

--- app/src/main/kotlin/nl/eduid/screens/editname/EditNameFormScreen.kt
+++ app/src/main/kotlin/nl/eduid/screens/editname/EditNameFormScreen.kt
@@ -1,25 +1,21 @@
 package nl.eduid.screens.editname
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,13 +32,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.flowWithLifecycle
@@ -133,33 +127,21 @@ fun EditNameFormContent(
     val submitEnable by remember(isGivenNameValid, isFamilyNameValid) {
         derivedStateOf { isGivenNameValid && isFamilyNameValid }
     }
-    val isKeyboardOpen by rememberUpdatedState(WindowInsets.isImeVisible)
     Column(
         horizontalAlignment = Alignment.Start, modifier = Modifier.fillMaxWidth()
     ) {
         val keyboardController = LocalSoftwareKeyboardController.current
         val focusRequester = remember { FocusRequester() }
-        AnimatedVisibility(
-            !isKeyboardOpen, Modifier.fillMaxWidth()
-        ) {
-            TwoColorTitle(
-                firstPart = stringResource(R.string.EditName_Title_Edit_COPY),
-                secondPart = stringResource(R.string.EditName_Title_YourName_COPY)
-            )
-            Spacer(Modifier.height(12.dp))
-        }
+        TwoColorTitle(
+            firstPart = stringResource(R.string.EditName_Title_Edit_COPY),
+            secondPart = stringResource(R.string.EditName_Title_YourName_COPY)
+        )
+        Spacer(Modifier.height(12.dp))
         if (inProgress) {
             LinearProgressIndicator(
                 modifier = Modifier.fillMaxWidth()
             )
-            Spacer(Modifier.height(12.dp))
         }
-
-        Text(
-            style = MaterialTheme.typography.bodyLarge.copy(textAlign = TextAlign.Start),
-            text = stringResource(R.string.Email_Info_COPY),
-            modifier = Modifier.fillMaxWidth()
-        )
         Spacer(Modifier.height(12.dp))
 
         OutlinedTextField(
