c75b3551e5040e60bda2937034e28220b6812112
Iulia STANA <iulia@egeniq.com>
2024-08-23 16:30:39 +0200

    TIQR-460: handle no connection Add interceptor Small cleanup from review

--- app/src/main/kotlin/nl/eduid/di/module/EduIdModule.kt
+++ app/src/main/kotlin/nl/eduid/di/module/EduIdModule.kt
@@ -20,6 +20,7 @@ import nl.eduid.di.auth.TokenProvider
 import nl.eduid.di.repository.EduIdRepository
 import nl.eduid.di.repository.StorageRepository
 import nl.eduid.env.EnvironmentProvider
+import nl.eduid.network.ConnectivityInterceptor
 import nl.eduid.screens.personalinfo.PersonalInfoRepository
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -31,9 +32,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
-/**
- * Module which serves the repositories.
- */
+/** Module which serves the repositories. */
 @Module
 @InstallIn(SingletonComponent::class)
 internal object RepositoryModule {
@@ -86,12 +85,13 @@ internal object RepositoryModule {
         okHttpClient: OkHttpClient,
     ): OkHttpClient {
         val builder = okHttpClient.newBuilder()
-        if (BuildConfig.BUILD_TYPE == "debug") {
+        if (BuildConfig.DEBUG) {
             builder.addInterceptor(loggingInterceptor)
         }
 
         return builder.addInterceptor(tokenInterceptor)
             .authenticator(tokenAuthenticator)
+            .addInterceptor(ConnectivityInterceptor())
             .build()
     }
 
