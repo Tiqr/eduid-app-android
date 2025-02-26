4a221abb741f63036b0fe3f5c25f746a2b1b2370
Dániel Zolnai <daniel@egeniq.com>
2025-02-26 15:06:44 +0100

    TIQR-522: Handle ErrorData not being saveable

--- app/src/main/kotlin/nl/eduid/ErrorData.kt
+++ app/src/main/kotlin/nl/eduid/ErrorData.kt
@@ -1,17 +1,20 @@
 package nl.eduid
 
 import android.content.Context
+import android.os.Parcelable
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Stable
+import kotlinx.parcelize.Parcelize
 
 @Stable
+@Parcelize
 data class ErrorData(
     val title: String = "",
     val message: String = "",
     @StringRes private val titleId: Int = 0,
     @StringRes private val messageId: Int = 0,
     private val messageArg: String? = null,
-) {
+) : Parcelable {
     fun title(context: Context): String = title.ifEmpty { context.getString(titleId) }
     fun message(context: Context): String =
         message.ifEmpty {
